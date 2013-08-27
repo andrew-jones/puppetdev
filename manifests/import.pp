71fcb94e387e0db6d9e817df59bfd7596c4dac12
Tim Brown <timb@softint.com.au>
2013-08-27 12:03:51 +1000

    Add import dependencies

--- manifests/import.pp
+++ manifests/import.pp
@@ -4,3 +4,15 @@ package {'perl-Log-Log4perl':
 package {'perl-Date-Calc':
 	ensure => installed
 }
+package {'lftp':
+	ensure => installed
+}
+package {'perl-Text-CSV_XS':
+	ensure => installed
+}
+package {'perl-DBIx-Class':
+	ensure => installed
+}
+package {'perl-DBIx-Class-Schema-Loader':
+	ensure => installed
+}
